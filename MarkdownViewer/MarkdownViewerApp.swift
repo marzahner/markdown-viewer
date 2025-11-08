00491101fa11f554fcb7b5fabcacbbe76c4d5313
nuss <andreasnuss99@gmail.com>
2025-11-08 12:09:53 +0100

    Initial Commit

---
+++ MarkdownViewer/MarkdownViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownViewerApp.swift
+//  MarkdownViewer
+//
+//  Created by Andreas Nuss on 08.11.25.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
